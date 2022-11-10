4c8dfa283dc39b83faecbb6dd37660461c890d51
landycodes <landryandrewsk8@gmail.com>
2022-11-10 10:16:12 -0700

    delete function

--- public/js/dashboard/newpost.js
+++ public/js/dashboard/newpost.js
@@ -22,22 +22,3 @@ const newBlog = async (event) => {
   };
 
   document.querySelector('#blogpost').addEventListener('submit', newBlog);
-
-
-  const deleteButton = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-  
-      const response = await fetch(`/api/blog/${id}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert('Failed to delete project');
-      }
-    }
-  };
-
-  document.querySelector('.deleteBtn').addEventListener('click', deleteButton)
